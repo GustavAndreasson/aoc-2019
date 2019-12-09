bb601236e1fbdf530824b61cde1a538dddce2faa
Gustav Andréasson <gustav.andreasson@ateles.se>
2019-12-09 10:46:07 +0100

    dag 8 del 2

--- dag8.js
+++ dag8.js
@@ -4,3 +4,8 @@ c=[]
 while(b.length>1)c.push(b.splice(0,150))
 bl=c.reduce((best,l)=>l.filter(x=>x==0).length<best.filter(x=>x==0).length?l:best)
 bl.filter(x=>x==1).length*bl.filter(x=>x==2).length
+
+//Part 2
+d=""
+for(y=0;y<6;y++){for(x=0;x<25;x++){for(z=0;z<100;z++){if(c[z][y*25+x]!="2"){d+=c[z][y*25+x];break;}}}d+="\n"}
+console.log(d)
