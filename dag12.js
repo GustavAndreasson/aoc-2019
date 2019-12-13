f7b00651c1c11f56496c8c72acd54db8931300a1
Gustav Andréasson <gustav.andreasson@ateles.se>
2019-12-13 09:02:08 +0100

    lade in lägsta gemensamma multipel utan fusk

--- dag12.js
+++ dag12.js
@@ -18,4 +18,6 @@ document.getElementsByTagName("pre")[0].innerText.split("\n").forEach(s=>{let e=
 let rots=[0,0,0];
 let checkSame=dim=>pos.every((p,i)=>p[dim]==spos[i][dim]&&vel[i][dim]==0)
 for(let i=0;i<1000000;i++){updateVel();pos=updatePos();rots=[0,1,2].map(d=>checkSame(d)?i+1:rots[d]);if(rots.every(r=>r!=0))break;}
-//Found online calculator for finding least common multiple from the numbers i rots
+let gcd=(a,b)=>!b?a:gcd(b,a%b);
+let lcm=(a,b)=>a*(b/gcd(a,b));
+rots.reduce((r,x)=>lcm(r,x),1)
