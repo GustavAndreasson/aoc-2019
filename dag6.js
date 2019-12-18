3860576677680d5270f2cacb65d0c137d6c37b26
Gustav Andréasson <gustav.andreasson@ateles.se>
2019-12-18 11:04:56 +0100

    dag 6

--- dag6.js
+++ dag6.js
@@ -1,6 +1,6 @@
 //Part 1
 let obs={}
-document.getElementsByTagName("pre")[0].innerText.split("\n").forEach(s=>{if(s)obs[s.match(/^(...)\)(...)$/)[2]]=s.match(/^(...)\)(...)$/)[1]})
+document.getElementsByTagName("pre")[0].innerText.split("\n").map(s=>s.match(/^(...)\)(...)$/)).forEach(s=>{if(s)obs[s[2]]=s[1]})
 let count=o=>obs[o]?1+count(obs[o]):0
 Object.keys(obs).map(count).reduce((s,v)=>s+v)
 
