e9bad3813dd08cfffba9bad0e4bf90e3d243ffae
Gustav Andréasson <gustav.andreasson@ateles.se>
2019-12-18 14:57:01 +0100

    dag 16

--- dag16.js
+++ dag16.js
@@ -1,13 +1,11 @@
 //Part 1
-let m=document.getElementsByTagName("pre")[0].innerText.split("").map(x=>parseInt(x));
+let m=document.getElementsByTagName("pre")[0].innerText.trim().split("").map(x=>parseInt(x));
 let pat=[0,1,0,-1]
-m.pop();
 for(let i=0;i<100;i++)m=m.map((digit,i,arr)=>Math.abs(arr.slice(i).reduce((sum,d,j)=>sum+d*pat[Math.floor((j)/(i+1)+1)%4]))%10);
 m.slice(0,8).join("");
 
 //Part 2
-m=document.getElementsByTagName("pre")[0].innerText.split("").map(x=>parseInt(x));
-m.pop();
+m=document.getElementsByTagName("pre")[0].innerText.trim().split("").map(x=>parseInt(x));
 let offset=parseInt(m.slice(0,7).join(""))
 let s=[];
 for(let i=0;i<10000;i++)s.push(m);
