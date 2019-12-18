35c287225a80fba6e11e34dfbebf7fa923f7f065
Gustav Andréasson <gustav.andreasson@ateles.se>
2019-12-18 10:59:59 +0100

    dag 4

--- dag4.js
+++ dag4.js
@@ -1,7 +1,7 @@
 //Part 1
-let test=n=>(n.toString().split("").filter((c,i,a)=>i==0||c>=a[i-1]).length==6)&&(n.toString().split("").filter((c,i,a)=>i!=0&&c==a[i-1]).length>0)
+let test=n=>n.toString().split("").every((c,i,a)=>i==0||c>=a[i-1])&&n.toString().split("").some((c,i,a)=>i!=0&&c==a[i-1])
 Array(746325-264360+1).fill().map((_,i)=>i+264360).filter(test).length
 
 //Part 2
-test=n=>(n.toString().split("").filter((c,i,a)=>i==0||c>=a[i-1]).length==6)&&(n.toString().split("").filter((c,i,a)=>i!=0&&c==a[i-1]&&(i<=1||c!=a[i-2])&&(i>=5||c!=a[i+1])).length>0)
+test=n=>n.toString().split("").every((c,i,a)=>i==0||c>=a[i-1])&&n.toString().split("").some((c,i,a)=>i!=0&&c==a[i-1]&&(i<=1||c!=a[i-2])&&(i>=5||c!=a[i+1]))
 Array(746325-264360+1).fill().map((_,i)=>i+264360).filter(test).length
