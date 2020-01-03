88285a914a059d6d2112e499056760aea653494f
Gustav Andréasson <gustav.andreasson@ateles.se>
2020-01-03 10:51:23 +0100

    dag 8

--- dag8.js
+++ dag8.js
@@ -6,6 +6,5 @@ bl=c.reduce((best,l)=>l.filter(x=>x==0).length<best.filter(x=>x==0).length?l:bes
 bl.filter(x=>x==1).length*bl.filter(x=>x==2).length
 
 //Part 2
-d=""
+d="\n"
 for(y=0;y<6;y++){for(x=0;x<25;x++){for(z=0;z<100;z++){if(c[z][y*25+x]!="2"){d+=c[z][y*25+x];break;}}}d+="\n"}
-console.log(d)
