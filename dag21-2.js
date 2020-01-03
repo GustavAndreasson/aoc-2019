b8df8fb69a1e0103508a0dc3a88b258835b6edbf
Gustav Andréasson <gustav.andreasson@ateles.se>
2020-01-03 10:29:41 +0100

    dag 21

---
+++ dag21-2.js
@@ -0,0 +1,41 @@
+//Part 2
+
+let pp=0;
+let rb=0;
+let codes={
+    1:{np:3, run:(a,b,c)=>{prog[c]=a+b;pp+=4}},
+    2:{np:3, run:(a,b,c)=>{prog[c]=a*b;pp+=4}},
+    3:{np:1, run:(a)=>{prog[a]=input.shift();pp+=2}},
+    4:{np:1, run:(a)=>{output=a;pp+=2}},
+    5:{np:2, run:(a,b)=>{if(a){pp=b}else{pp+=3}}},
+    6:{np:2, run:(a,b)=>{if(!a){pp=b}else{pp+=3}}},
+    7:{np:3, run:(a,b,c)=>{prog[c]=a<b?1:0;pp+=4}},
+    8:{np:3, run:(a,b,c)=>{prog[c]=a==b?1:0;pp+=4}},
+    9:{np:1, run:(a)=>{rb+=a;pp+=2}},
+    99:{np:0, run:()=>{console.log("----------------SLUT------------");throw(new Exception);}}
+}
+let getCmd=ptr=>parseInt(prog[ptr].toString().substr(-2))
+let getParams=ptr=>prog.slice(ptr+1,ptr+1+codes[getCmd(ptr)].np).map((v,i)=>{
+    let mode=prog[ptr].toString().charAt(prog[ptr].toString().length-3-i);
+    if (mode=="1") return v;
+    if (mode=="2") return (i==codes[getCmd(ptr)].np-1&&getCmd(ptr)!=4&&getCmd(ptr)!=5&&getCmd(ptr)!=6&&getCmd(ptr)!=9)?rb+v:prog[rb+v]||0;
+    return (i==codes[getCmd(ptr)].np-1&&getCmd(ptr)!=4&&getCmd(ptr)!=5&&getCmd(ptr)!=6&&getCmd(ptr)!=9)?v:prog[v]||0;
+})
+let prog=document.getElementsByTagName("pre")[0].innerText.split(",").map(v=>parseInt(v));
+let output=null,input=[];
+let string="";
+input="NOT A T\nNOT T T\nAND B T\nAND C T\nNOT T J\nAND D J\nNOT H T\nNOT T T\nOR E T\nAND T J\nRUN\n".split("").map(l=>l.charCodeAt(0));
+while(getCmd(pp)!=99&&pp<prog.length) {
+    codes[getCmd(pp)].run(...getParams(pp));
+	if (output!=null) {
+		if (output==10) {
+			console.log(string);
+			string="";
+		} else if (output < 256) {
+			string += String.fromCharCode(output);
+		} else {
+			console.log("RESULT:"+output);
+		}
+		output=null;
+	}
+}
